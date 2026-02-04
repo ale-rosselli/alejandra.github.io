c93fd71c05b238e1fe55d8d742554501176e9f88
Alejandra Rosselli-Calderon <40182566+ale-rosselli@users.noreply.github.com>
2026-02-03 16:15:05 -0800

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});
               opacity:8,speed:3},repulse:{distance:200,duration:.4},push:{particles_nb:4},remove:{particles_nb:2}}},retina_detect:!0});
 
 particlesJS("particles-js2",{particles:{number:{value:30,density:{enable:!0,value_area:800}},color:{value:"#ffffff"},
-              shape:{type:"polygon",stroke:{width:0,color:"#000000"},polygon:{nb_sides:5},image:{src:"img/github.svg",width:100,height:100}},
+              shape:{type:"polygon",stroke:{width:0,color:"#000000"},polygon:{nb_sides:5},image:{src:"img/github.svg",width:100,height:40}},
               opacity:{value:.5,random:!1,anim:{enable:!1,speed:1,opacity_min:.1,sync:!1}},size:{value:3,random:!0,
               anim:{enable:!1,speed:19.18081918081918,size_min:.1,sync:!1}},line_linked:{enable:!0,distance:150,color:"#ffffff",opacity:.4,width:1},
               move:{enable:!0,speed:4,direction:"none",random:!0,straight:!1,out_mode:"out",bounce:!1,
